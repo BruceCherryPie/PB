d0a7e8438c740ee52b146da36e9c7c2f378d5357
Савельевы <pro100borya@mail.ru>
2020-10-16 22:51:10 +0600

    10.16 v1

---
+++ views/contacts/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Список контактов';
+?>
+<?php
+var_dump('contacts');
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h1>Список контактов</h1>
+
+    </div>
+
+</div>
